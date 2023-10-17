732cb594f96b5731b016b246e359f20671b5fc62
AndriiKot <a_kocyuba@ukr.net>
2023-10-17 19:24:04 +0100

    continuation of the tutorial TypeScript

---
+++ 2023.10.17/typescript book/example 1/example 1.js
@@ -0,0 +1,16 @@
+var first_var = 'I am string';
+var second_var = 143;
+var isOld = false;
+var some_var3 = null;
+var some_var4 = undefined;
+var first_array = [
+    first_var,
+    second_var,
+    isOld,
+    some_var3,
+    some_var4
+];
+for (var index in first_array) {
+    console.dir(first_array[index]);
+}
+;
