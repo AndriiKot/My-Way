460c22b4cc36b13affc7df9ca726b325b17b0986
AndriiKot <a_kocyuba@ukr.net>
2023-10-18 14:18:03 +0100

    example 3.js solved*

--- 2023.10.18/js book/example 3.js
+++ 2023.10.18/js book/example 3.js
@@ -5,3 +5,13 @@ const myArray = [11, 22, 33, 44];
 for(const element of myArray) {
     console.dir(element);
 };
+
+
+let amount = 0
+
+for(const element of myArray){
+    amount += element;
+}
+
+console.log(`amount: ${amount}`);
+
