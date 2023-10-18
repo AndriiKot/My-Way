5f8c437175f207311cc1dc02d874a82c4c1376a8
AndriiKot <a_kocyuba@ukr.net>
2023-10-18 14:39:34 +0100

    example 4.js solved*

---
+++ 2023.10.18/js book/example 4.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+
+const fn1 = async () => {
+    console.log(`Hello I am function:\n ${fn1}!`)
+};
+
+const main = async () => {
+    const res = await fn1();
+    return res;
+};
+
+
+console.log(main());
+
+
