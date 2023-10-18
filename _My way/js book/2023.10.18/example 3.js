a9c0073196c8cafce12768047329305309f8d84e
AndriiKot <a_kocyuba@ukr.net>
2023-10-18 14:42:12 +0100

    example 1-4 solved

---
+++ _My way/js book/2023.10.18/example 3.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+const myArray = [11, 22, 33, 44];
+
+for(const element of myArray) {
+    console.dir(element);
+};
+
+
+let amount = 0
+
+for(const element of myArray){
+    amount += element;
+}
+
+console.log(`amount: ${amount}`);
+
