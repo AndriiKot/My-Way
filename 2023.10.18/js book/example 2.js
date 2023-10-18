cbecc4938c1c38870a99393f051a51c951cf9011
AndriiKot <a_kocyuba@ukr.net>
2023-10-18 13:59:34 +0100

    test fn1 and fn2

---
+++ 2023.10.18/js book/example 2.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+
+const fn1 = (a = 0,b = 0) => (a + b);
+const fn2 = (a,b) => { 
+    a = a || 0;
+    b = b || 0;
+    return a + b
+};
+
+console.dir(fn1(1,3));
+console.dir(fn2(4,2));
