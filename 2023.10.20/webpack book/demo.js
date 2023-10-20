16b90f7af927409f9d801e644ee3408cca5c9e09
AndriiKot <a_kocyuba@ukr.net>
2023-10-20 18:27:45 +0100

    add function component

---
+++ 2023.10.20/webpack book/demo.js
@@ -0,0 +1,15 @@
+﻿// 7. Создайте вайл js. В котором функция будет 
+//     *создовать html елемент 
+//     *затем довабете в этот эелемент строку
+//     *строку образуйте при помоши преоброзования массива ['Hello','webpack'] межу елементами массива должен ити один пробел.
+//     *функции должна вернуть новый html елемент
+
+function myfn1() {
+    const element = document.createElement('div');
+    element.innerHTML = _.join(['Hello','webpack'],' ');
+    return element;
+}
+
+document.body.appendElement(myfn1());
+
+
