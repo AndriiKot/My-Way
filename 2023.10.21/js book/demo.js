268b64a7f35182236b7a22318ac3327d33ee5b63
AndriiKot <a_kocyuba@ukr.net>
2023-10-21 10:19:44 +0100

    add call await functions

--- 2023.10.21/js book/demo.js
+++ 2023.10.21/js book/demo.js
@@ -12,4 +12,11 @@ const fn3 = () => {
     console.log('Hello, fn3');
 };
 
+console.log('Old syntax: ');
 fn1(fn2(fn3()));
+
+console.log('New syntax: ');
+
+await fn3();
+await fn2();
+await fn1();
