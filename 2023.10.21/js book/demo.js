41763e957cff6560f72f3026faf768ced697585d
AndriiKot <a_kocyuba@ukr.net>
2023-10-21 10:09:44 +0100

    continuation of the tutorial 'js book'

---
+++ 2023.10.21/js book/demo.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+// const fn1 = (text, (data) => {
+//  const fn3 = (data,(newData) => {
+//    const fn3 = (newData,() => {
+//         console.log('fn3')
+//     });
+//   });
+// }) => {};
+
+const fn1 = () => {
+    console.log("Hello, fn1");
+};
+
+fn1();
