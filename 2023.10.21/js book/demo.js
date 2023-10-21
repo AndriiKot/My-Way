3982adb77d74c195983c17d163a7215fc794b984
AndriiKot <a_kocyuba@ukr.net>
2023-10-21 10:14:43 +0100

    create async code

--- 2023.10.21/js book/demo.js
+++ 2023.10.21/js book/demo.js
@@ -12,4 +12,12 @@ const fn1 = () => {
     console.log("Hello, fn1");
 };
 
-fn1();
+const fn2 = () => {
+    console.log('Hello, fn2');
+};
+
+const fn3 = () => {
+    console.log('Hello, fn3');
+};
+
+fn1(fn2(fn3()));
