ce9d1238c68b517474643a3f2e3c744e665a5ed9
AndriiKot <a_kocyuba@ukr.net>
2023-10-25 10:26:13 +0100

    ennding part 1 of 3 of the tutorial 'dom api book'

--- 2023.10.25/dom api book/script.js
+++ 2023.10.25/dom api book/script.js
@@ -1,3 +1,19 @@
 ﻿'use strict';
 
-console.log(important-heading);
+let my_first_paragraph = 13;
+console.log(important_heading);
+
+/* ERROR: 
+!!! console.log(not-inportant-heading); !!!
+*/
+
+const not_commets = /*I am a comment*/'It is not a comments ' ;
+console.log(not_commets); // 'It is not a comments '
+
+console.log(my_first_paragraph); // 13
+
+const my_paragraph = document.getElementById('my_first_paragraph');
+console.dir({my_paragraph}); // Object HTMLParagraphElement
+
+
+
