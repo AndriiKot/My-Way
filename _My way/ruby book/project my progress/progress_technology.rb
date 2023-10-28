22489cb079e802db3f2fcbd8089cc011ae6bcd3d
AndriiKot <a_kocyuba@ukr.net>
2023-10-28 17:34:06 +0100

    continuation of the tutorial 'ruby book'

--- _My way/ruby book/project my progress/progress_technology.rb
+++ _My way/ruby book/project my progress/progress_technology.rb
@@ -1,32 +1,36 @@
-﻿daysprogress = File.readlines('progress.txt')
+﻿daysprogress = File.readlines('delete_commas.txt')
 days_training = 0;
 
 hash_technologies = Hash.new(0)
 
 daysprogress.each do |line|
-    str_technologie = ''
-
+date_format = false
+hours = false
+str_key = ''
+str_value = ''
     line.each_char do |chr|
-      case chr
-       when 'a'..'z' then str_technologie += chr
-      else  
-           if str_technologie != ''
-            hash_technologies[str_technologie] = 0 
-           else
-             next
-           end
-           str_technologie = ''
-      end              
+      if (date_format || ' ' ) then next end
+      if (chr == "(") then date_format = true end
+      if (chr == "\n") then date_format = false; next end
+      if ('a'..'z' === chr) then str_key += chr; next end
+      if (chr == "[")
+        hash_technologies[str_key] += 0
+        str_key = ''
+        hours = true
+      end
+      if (hours and chr != "]") then str_value += chr; next end
+      if (chr == "]")
+         hash_technologies[str_key] += str_value.to_f
+         str_value = ''
+         hours = false
+         next
+      end
     end
-
 end
 
 
 
-hash_technologies.keys.sort!.each do |technologi|
-    puts "technology: #{technologi}"
-end
-
+p hash_technologies
 puts "Total: #{hash_technologies.size}"
 
 
