dd00bafe328710bd86629f7d89f23073ed4ef3af
AndriiKot <a_kocyuba@ukr.net>
2023-10-29 10:33:45 +0100

    continuation of the tutorial 'ruby book'

---
+++ _My way/ruby book/2023.10.28/progress_technology.rb
@@ -0,0 +1,40 @@
+﻿daysprogress = File.readlines('progress.txt')
+days_training = 0;
+
+hash_technologies = Hash.new(0)
+
+daysprogress.each do |line|
+    str_technologie = ''
+
+    line.each_char do |chr|
+      case chr
+       when 'a'..'z' then str_technologie += chr
+      else  
+           if str_technologie != ''
+            hash_technologies[str_technologie] = 0 
+           else
+             next
+           end
+           str_technologie = ''
+      end              
+    end
+
+end
+
+
+file_t = File.new("Technologi.txt", 'w+')
+hash_technologies.keys.sort!.each do |technologi|
+    puts "technology: #{technologi}"
+    file_t.puts "#{technologi}"
+end
+
+file_t.close
+
+puts "Total: #{hash_technologies.size}"
+
+
+
+
+
+
+
