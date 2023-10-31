b8f0c0a81158d7e617b1ca4492d2c1605baf1cbd
AndriiKot <a_kocyuba@ukr.net>
2023-10-31 19:29:37 +0100

    continuation of the tutorial 'ruby book'

---
+++ 2023.10.31/js book/setInterval.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+const INTERVAL = 500;
+const MAX_VALUE = 10;
+
+let counter = 0;
+let timer = null;
+
+const event = () => {
+    if(counter === MAX_VALUE || counter > MAX_VALUE){
+      console.log('The End');
+      clearInterval(timer)
+      return;
+    }
+    console.dir({ counter, date: new Date() });
+    counter++;
+};
+
+console.log('Begin');
+timer = setInterval(event,INTERVAL);
