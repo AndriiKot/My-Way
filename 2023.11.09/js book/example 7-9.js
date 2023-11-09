f842760ec4be7eca9a3641f91d16a6bc78f81e65
AndriiKot <a_kocyuba@ukr.net>
2023-11-09 21:46:23 +0100

    continuation tutorial 'javascript book' and 'ruby book'

---
+++ 2023.11.09/js book/example 7-9.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+const purchase = {
+  Electronics: [
+    { name: "Laptop", price: 1500 },
+    { name: "Keyboard", price: 100 },
+    { name: "HDMI cable", price: 10 },
+  ],
+  Textile: [{ name: "Bag", price: 50 }],
+};
+// 7
+const electronics = purchase.Electronics;
+console.log(electronics);
+
+const textile = purchase["Textile"];
+console.log(textile);
+
+//8
+const bag = textile[0];
+console.log(bag);
+
+const price = electronics.at(-1).price
+console.log(price)
+
+// 9 
+const json = JSON.stringify(purchase);
+console.log(json);
+
+const obj = JSON.parse(json);
+console.log(obj);
+
+
