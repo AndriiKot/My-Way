fa70fab1ba64dc3b449fdac161f3987753c7c7b6
AndriiKot <a_kocyuba@ukr.net>
2023-12-09 23:49:49 +0100

    continuation tutorial 'js book'

---
+++ _My way/ECMA/dom api book/Chronology/2023.12.09/index.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+const p1 = document.querySelector('.p1')
+const p2 = document.querySelector('.p2')
+
+const paragraphs = document.querySelectorAll('p')
+
+
+paragraphs.forEach((p, i) => {
+    p.innerText = ("Hello " + (i + 1) +'\n' + p.innerText);
+});
+
