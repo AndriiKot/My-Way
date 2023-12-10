cfa123a50b127ed3ce08f5d70310899b276737e4
AndriiKot <a_kocyuba@ukr.net>
2023-12-10 17:10:04 +0100

    continuation tutorial 'js book'

---
+++ _My way/ECMA/рецепты/Перебор NodeList for/index.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+const p1 = document.querySelector('.p1');
+const p2 = document.querySelector('.p2');
+
+const paragraphs = document.querySelectorAll('p');
+
+const len = paragraphs.length;
+
+for(let i = 0; i < len; i++) {
+    const el = paragraphs[i];
+    const text = paragraphs[i].innerText;
+    const addNewText = "Hello " + (i + 1) + '\n';
+    el.innerText = addNewText + text;
+    if ( i == 1) {
+        el.style.background = 'blue';
+    }
+}
+
+
+
+
+
