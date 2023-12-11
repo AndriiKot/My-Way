a72213438b814661254e0032464fe64c033aaaf4
AndriiKot <a_kocyuba@ukr.net>
2023-12-11 10:27:44 +0100

    continuation tutorial 'ts book'

--- _My way/ECMA/Languages/typescript book/Net Ninja TS/2023.12.10/index.js
+++ _My way/ECMA/Languages/typescript book/Net Ninja TS/2023.12.10/index.js
@@ -2,7 +2,11 @@ var age = 40;
 var userName = "Andrii";
 var isFunctional;
 userName = 'NewName';
+age = 38;
 isFunctional = false;
 console.log(age);
 console.log(userName);
 console.log(isFunctional);
+isFunctional = true;
+console.log(isFunctional);
+
--- _My way/ECMA/Languages/typescript book/Net Ninja TS/2023.12.10/index.ts
+++ _My way/ECMA/Languages/typescript book/Net Ninja TS/2023.12.10/index.ts
@@ -4,8 +4,14 @@ let isFunctional: boolean;
 
 
 userName = 'NewName';
+age = 38;
 isFunctional = false;
 
 console.log(age);
 console.log(userName);
 console.log(isFunctional);
+
+isFunctional = true;
+console.log(isFunctional)
+
+ 
