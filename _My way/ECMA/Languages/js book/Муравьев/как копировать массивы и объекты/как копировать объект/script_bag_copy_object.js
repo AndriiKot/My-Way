2e3bde66a1d21ff1e6ce99e145a46f2fc785769e
AndriiKot <a_kocyuba@ukr.net>
2023-12-14 11:33:22 +0100

    js learning from Muravyov copy objects

---
+++ _My way/ECMA/Languages/js book/Муравьев/как копировать массивы и объекты/как копировать объект/script_bag_copy_object.js
@@ -0,0 +1,22 @@
+﻿'use sctrict';
+
+const person = {
+  name: 'Roma',
+  age: 33,
+}
+
+const manager = person;
+manager.age = 39;
+
+console.dir({person});  // { person: { name: 'Roma', age: 39 } } !!!
+console.dir({manager}); // { manager: { name: 'Roma', age: 39 } } !!!
+
+
+
+
+
+
+
+
+
+
---
+++ _My way/ECMA/Languages/js book/Муравьев/как копировать массивы и объекты/как копировать объект/script_spread_copy_object.js
@@ -0,0 +1,25 @@
+﻿'use sctrict';
+
+const person = {
+  name: 'Roma',
+  age: 33,
+}
+
+
+const manager_1 = {...person};
+manager_1.age = 39;
+
+const manager_2 = {...person, name: 'Andrii', age: 38, height: 1.87 };
+
+console.dir({person});   // { person: { name: 'Roma', age: 33 } }
+console.dir({manager_1}); // { manager_1_2: { name: 'Roma', age: 39 } }
+console.dir({manager_2}); // { manager: { name: 'Andrii', age: 38, heigth: 1.87 } }
+
+
+
+
+
+
+
+
+
