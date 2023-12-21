c75055afa770f5713b5adead3dc6b91712a5ec6a
AndriiKot <a_kocyuba@ukr.net>
2023-12-21 21:43:30 +0100

    modules js

---
+++ _My way/ECMA/Languages/js book/Муравьев/модули/lessons/lesson 5/app.js
@@ -0,0 +1,7 @@
+"use strict";
+import getCities from "./src/constants";
+
+console.log(getCities)
+
+console.log("HI!!!")
+
---
+++ _My way/ECMA/Languages/js book/Муравьев/модули/lessons/lesson 5/src/constants.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+import axios from "axios";
+
+export const citiesUrl =
+  "https://gist.githubusercontent.com/VasilyMur/8b679b5482499a97c08cca9b6dffef03/raw/4dfd59daa4ac0a6ebd7d2f144b860d62054b4576/uscities.json";
+
+export const mapCities = (cities) => {
+  return cities.map((response) => {
+    const { city, population } = response;
+    return { city, population };
+  });
+};
+
+const getCities = () => {
+  return axios.get(citiesUrl).then((res) => {
+    return res.data;
+  });
+};
+
+export default getCities;
