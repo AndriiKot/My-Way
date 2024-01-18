457b6d2611f0e6b94277519b8c9e52e92e737dbf
AndriiKot <a_kocyuba@ukr.net>
2024-01-18 12:32:41 +0100

    create new component

---
+++ FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/MyComponentVue.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>My Component Vue</h1>
+</template>
--- FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/main.js
+++ FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import MyComponent from './MyComponentVue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component('my-component', MyComponent);
+app.mount('#app');
