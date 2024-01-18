c3c18c6bac7b3d67c77a3cac1a55db7b80ea6368
AndriiKot <a_kocyuba@ukr.net>
2024-01-18 12:54:24 +0100

    instructions for connecting the component

--- FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/main.js
+++ FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import MyComponentVue from './MyComponentVue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component('my-component', MyComponentVue);
+app.mount('#app');
