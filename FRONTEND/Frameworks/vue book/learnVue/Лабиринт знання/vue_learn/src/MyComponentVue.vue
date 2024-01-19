eeb74829d170c0294ef0a5b6c638dc27a1586068
AndriiKot <a_kocyuba@ukr.net>
2024-01-19 18:57:31 +0100

    delete type='button'

--- FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/MyComponentVue.vue
+++ FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/MyComponentVue.vue
@@ -2,8 +2,8 @@
   <div>
     <h1>My Component Vue</h1>
     <h1>Счётчик: {{ counter }}</h1>
-    <button type="button" class="btn btn-danger">Уменьшить</button>
-    <button type="button" class="btn btn-success">Увеличить</button>
+    <button class="btn btn-danger">Уменьшить</button>
+    <button class="btn btn-success">Увеличить</button>
   </div>
 </template>
 
