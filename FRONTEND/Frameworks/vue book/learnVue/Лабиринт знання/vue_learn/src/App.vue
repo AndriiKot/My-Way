6947c253bf005d3664293573537e358d447c29d6
AndriiKot <a_kocyuba@ukr.net>
2024-01-19 18:55:50 +0100

    move buttons

--- FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/App.vue
+++ FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <h1>App</h1>
   <my-component />
-  <button type="button" class="btn btn-danger">Danger</button>
-  <button type="button" class="btn btn-success">Success</button>
 </template>
 
 <script></script>
--- FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/MyComponentVue.vue
+++ FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/MyComponentVue.vue
@@ -1,14 +1,18 @@
 <template>
+  <div>
     <h1>My Component Vue</h1>
     <h1>Счётчик: {{ counter }}</h1>
+    <button type="button" class="btn btn-danger">Уменьшить</button>
+    <button type="button" class="btn btn-success">Увеличить</button>
+  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        counter: 1
-      }
-    }
-  }
+export default {
+  data() {
+    return {
+      counter: 1,
+    };
+  },
+};
 </script>
