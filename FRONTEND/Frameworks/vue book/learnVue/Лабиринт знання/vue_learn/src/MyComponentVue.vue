0344cda25de7376273a0e54e0ee72c0ae51f809e
AndriiKot <a_kocyuba@ukr.net>
2024-01-19 21:13:23 +0100

    corect pritter

--- FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/MyComponentVue.vue
+++ FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/MyComponentVue.vue
@@ -2,8 +2,8 @@
   <div>
     <h1>My Component Vue</h1>
     <h1>Счётчик: {{ counter }}</h1>
-    <button class="btn btn-danger" v-on:click="counter--">Уменьшить</button>
-    <button class="btn btn-success" v-on:click="counter++">Увеличить</button>
+    <button class="btn btn-danger" v-on:click="decrease">Уменьшить</button>
+    <button class="btn btn-success" v-on:click="encrease">Увеличить</button>
   </div>
 </template>
 
@@ -13,6 +13,14 @@ export default {
     return {
       counter: 1,
     };
+  },
+  methods: {
+    encrease() {
+      this.counter += 1;
+    },
+    decrease() {
+      this.counter -= 1;
+    },
   },
 };
 </script>
