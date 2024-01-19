b29339b0559417a071c2b2e4554ff024c55acf25
AndriiKot <a_kocyuba@ukr.net>
2024-01-19 18:51:47 +0100

    add buttons bootstrap

--- FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/App.vue
+++ FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <h1>App</h1>
   <my-component />
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-success">Success</button>
 </template>
 
-<script>
-</script>
+<script></script>
 
-<style>
-</style>
+<style></style>
