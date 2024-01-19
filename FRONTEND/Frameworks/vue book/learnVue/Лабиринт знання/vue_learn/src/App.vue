204afd5390ad3eebdff1258ddcc7c78c2131a806
AndriiKot <a_kocyuba@ukr.net>
2024-01-19 18:11:55 +0100

    delete folder 'components'

--- FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/App.vue
+++ FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/App.vue
@@ -1,11 +1,8 @@
 <template>
   <h1>App</h1>
-  <my-component />
 </template>
 
 <script>
-
 </script>
-
 <style>
 </style>
--- FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/MyComponentVue.vue
+++ FRONTEND/Frameworks/vue book/learnVue/Лабиринт знання/vue_learn/src/MyComponentVue.vue
@@ -1,3 +1,14 @@
 <template>
-    <h1>My Component Vue</h1>
+    <!-- <h1>My Component Vue</h1> -->
+    <h1>Счётчик: {{ counter }}</h1>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        counter: 1
+      }
+    }
+  }
+</script>
