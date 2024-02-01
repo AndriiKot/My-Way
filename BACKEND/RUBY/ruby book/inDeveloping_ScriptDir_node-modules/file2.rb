6928cb1a97efe97393e6580a80efb4aeeee63d58
AndriiKot <a_kocyuba@ukr.net>
2024-02-01 12:21:31 +0100

    test not empty

--- BACKEND/RUBY/ruby book/inDeveloping_ScriptDir_node-modules/file2.rb
+++ BACKEND/RUBY/ruby book/inDeveloping_ScriptDir_node-modules/file2.rb
@@ -1,2 +1,10 @@
-﻿
-p ARGV
+﻿require 'fileutils'
+
+array_delete_folders = ARGV
+array_delete_folders.each_slice(2).to_a
+
+def eneter_path_and_folder 
+    p "HI !!"
+end
+
+array_delete_folders.size == 0 ? eneter_path_and_folder() : (print "NOt Empty!")
