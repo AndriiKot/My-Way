3cbc74502ae14b08298aafa071fb9d90a21dfb08
AndriiKot <a_kocyuba@ukr.net>
2024-03-08 00:36:04 +0100

    git docs and script_ruby

---
+++ script_ruby.rb
@@ -0,0 +1,9 @@
+﻿
+
+def removing_space_all_depth(folder_path = './')
+  Dir.glob(File.join(folder_path,"**","*")) do |path|
+    new_folder_or_file_name = path.strip.gsub(/[^\w[а-яА-Я]\.]/,'_')
+    File.rename(path, new_folder_or_file_name)
+  end
+end
+
