ff1b6643a1e9b076bd3d00c4d6b7176fc9529259
AndriiKot <a_kocyuba@ukr.net>
2024-03-08 21:03:17 +0100

    removing spaces in file and folder names

---
+++ BACKEND/NODEJS/NodeJs/РЕЦЕПТЫ/cmd_вывести_объект/вывести_объект.js
@@ -0,0 +1,26 @@
+﻿
+// console.dir(obj, { depth: null });
+
+//Example
+
+const purchase = {
+  Electronics: [
+    { name: 'Laptop', price: 1500 },
+    { name: 'Keyboadrd', price: 100 },
+    { name: 'HDMI cable', price: 10 },
+  ],
+  Textile: [
+    { name: 'Bag', price: 50 },
+  ],
+ };
+
+
+console.log(purchase);  // corect
+console.dir(purchase);  // corect
+console.dir(purchase, { depth: null });   // corect
+
+
+console.log({ purchase }); // [Object] !!!!
+
+
+
