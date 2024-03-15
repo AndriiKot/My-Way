e43a361b451f4e1237fe0807d4f5d9074134acfd
AndriiKot <a_kocyuba@ukr.net>
2024-03-15 23:58:33 +0100

    js docs

---
+++ FRONTEND/_Programming_Languages/JAVASCRIPT/NEW/return_boolean.js
@@ -0,0 +1,9 @@
+﻿function isEqual(a, b) {
+  return a === b;
+}
+
+function isLess(a, b) {
+  return a < b;
+}
+
+isLess(10, 15);
---
+++ FRONTEND/_Programming_Languages/JAVASCRIPT/NEW/ПОДУМАТЬ/switch.js
@@ -0,0 +1,54 @@
+﻿// Exapmle 1
+switch (a) {
+  case 3:
+    // some code
+    break;
+  case 4:
+    // some code
+    break;
+  case 5:
+    // some code
+    break;
+  default:
+  // some code
+}
+
+// Example 2
+// Если break нет, то выполнение
+// пойдёт ниже по следующим case, при этом остальные проверки игнорируются
+
+switch (a) {
+  case 3:
+    // some code
+  case 4:
+    // some code
+  case 5:
+    // some code
+  default:
+  // some code
+}
+
+// Example 3
+switch (a) {
+  case 3:
+  case 4:
+    // some code
+    break;
+  case 5:
+    // some code
+    break;  
+  default:
+  // some code
+} 
+
+// Example 4
+let a = '11';
+let b = 10;
+
+switch (+a) {
+  case b + 1:
+    // Выполнится так как а будет ссылаться на integer 11
+    break;
+  default:
+    alert("Это не выполнится");
+}
