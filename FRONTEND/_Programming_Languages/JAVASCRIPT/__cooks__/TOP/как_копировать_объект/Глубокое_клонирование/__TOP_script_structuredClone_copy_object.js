53a8c9946128048997a3da41ac000e32babf07c6
AndriiKot <a_kocyuba@ukr.net>
2024-03-21 22:41:05 +0100

    js docs

--- FRONTEND/_Programming_Languages/JAVASCRIPT/__cooks__/TOP/как_копировать_объект/Глубокое_клонирование/__TOP_script_structuredClone_copy_object.js
+++ FRONTEND/_Programming_Languages/JAVASCRIPT/__cooks__/TOP/как_копировать_объект/Глубокое_клонирование/__TOP_script_structuredClone_copy_object.js
@@ -5,15 +5,11 @@ const person = {
   age: 33,
 }
 
-
-const manager_1 = Object.assign({},person);
+const manager_1 = structuredClone(person);
 manager_1.age = 39;
-
-const manager_2 = Object.assign({}, person, {name: 'Andrii', age: 38, height: 1.87});
 
 console.dir({person});   // { person: { name: 'Roma', age: 33 } }
 console.dir({manager_1}); // { manager_1_2: { name: 'Roma', age: 39 } }
-console.dir({manager_2}); // { manager: { name: 'Andrii', age: 38, heigth: 1.87 } }
 
 
 
--- FRONTEND/_Programming_Languages/JAVASCRIPT/__cooks__/switch/__TOP__switch.js
+++ FRONTEND/_Programming_Languages/JAVASCRIPT/__cooks__/switch/__TOP__switch.js
@@ -1,7 +1,6 @@
 ﻿let count = 0;
 
 function cc(card) {
-  // Only change code below this line
     switch (card) {
         case 2:
         case 3:
@@ -19,8 +18,10 @@ function cc(card) {
             break;
     }
 
-  return "Change Me";
-  // Only change code above this line
+  if (count > 0) {
+    return count + " Bet";
+  }
+  return count + " Hold";
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
