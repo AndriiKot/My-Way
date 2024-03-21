ba2c744e119c75b1ce7871d3500d74d20134cab5
AndriiKot <a_kocyuba@ukr.net>
2024-03-21 10:59:20 +0100

    vue docs

---
+++ FRONTEND/Frameworks/VUE/learnVue/Лабиринт_знання/steps_origin/_2_создание_компонента_и_импортирование_его_в_проект_/step_2/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <MyComponentVue1 msg="My Fist Vue Component"/>
+</template>
+
+<script>
+import MyComponentVue1 from './components/MyComponentVue1.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyComponentVue1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ FRONTEND/Frameworks/VUE/learnVue/Лабиринт_знання/steps_origin/_2_создание_компонента_и_импортирование_его_в_проект_/step_2/src/components/MyComponentVue1.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>My {{ msg }}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'MyComponentVue1',
+    props: {
+      msg: String
+    }
+  } 
+</script>
