95ff88edceb84352b86d641746e6544abf783d5d
AndriiKot <a_kocyuba@ukr.net>
2024-03-21 23:02:02 +0100

    js docs

---
+++ FRONTEND/_Programming_Languages/JAVASCRIPT/__cooks__/objects/__TOP_Create_Objects.js
@@ -0,0 +1,23 @@
+﻿const anotherObject = {
+  make: "string",
+  5: "number",
+  model: "string",
+  model: "number",
+};
+
+//1. Свойство может иметь имя как строку, так и числовое значение
+// 2.Свойство может иметь двух одинаковых имен тогда оно будет
+//переписано на то значение которое указано вторым свойством
+
+// Свойство не может быть объектом
+
+console.dir(anotherObject);  // { '5': 'number', make: 'string', model: 'number' }
+
+console.dir(anotherObject[5]); // "number"
+console.dir(anotherObject["5"]); // "number"
+
+console.dir(anotherObject.model); // "number"
+console.dir(anotherObject["model"]); // "number"
+
+console.dir(anotherObject.make);
+console.dir(anotherObject["make"]);
