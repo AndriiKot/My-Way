1a504be1d4862c70c0d033b7d61b1627ceda123b
AndriiKot <a_kocyuba@ukr.net>
2024-03-26 10:26:01 +0100

    sql docs

---
+++ FRONTEND/_Programming_Languages/JAVASCRIPT/__cooks__/Functoons/Function_Declaration/вывести_тело_функции.js
@@ -0,0 +1,5 @@
+﻿function inc(a, b) {
+  return a + 1;
+}
+
+console.log(inc.toString());
---
+++ FRONTEND/_Programming_Languages/JAVASCRIPT/__cooks__/Functoons/вывести_тело_функцииNodeJS.js
@@ -0,0 +1,19 @@
+﻿function inc(a, b) {
+  return a + 1;
+}
+
+const sum = function (a, b) {
+  return a + b;
+};
+
+const max = (a, b) => (a > b ? a : b);
+
+const avg = (a, b) => {
+  const s = sum(a, b);
+  return s / 2;
+};
+
+console.log(inc.toString());
+console.log(sum.toString());
+console.log(max.toString());
+console.log(avg.toString());
