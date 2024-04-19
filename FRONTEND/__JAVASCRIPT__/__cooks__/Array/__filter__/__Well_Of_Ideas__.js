be39e017ad7077e8b9dd746cd46b7ce32be14d35
AndriiKot <a_kocyuba@ukr.net>
2024-04-19 09:31:38 +0200

    js docs

---
+++ FRONTEND/__JAVASCRIPT__/__cooks__/Array/__filter__/__Well_Of_Ideas__.js
@@ -0,0 +1,20 @@
+﻿const well1 = (x) => {
+  const good_count = x.filter((x) => x == "good").length;
+  return good_count < 1
+    ? "Fail!"
+    : good_count < 3
+    ? "Publish!"
+    : "I smell a series!";
+};
+
+function well2(x) {
+  switch (x.filter((i) => i === "good").length) {
+    case 0:
+      return "Fail!";
+    case 1:
+    case 2:
+      return "Publish!";
+    default:
+      return "I smell a series!";
+  }
+}
