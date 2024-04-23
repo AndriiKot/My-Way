a7ecea0ea084c17a72fb24b9d4ab9d8443891eb4
AndriiKot <a_kocyuba@ukr.net>
2024-04-23 18:22:22 +0200

    js docs

---
+++ FRONTEND/__JAVASCRIPT__/__cooks__/Array/__Array_FROM__/___Array_from___.js
@@ -0,0 +1,19 @@
+﻿function countBy1(x, n) {
+  const arr = Array.from({ length: n }, (_, i, step = x) => ((i * step) + x));
+  return arr;
+}
+
+const countBy = (x, n) => Array.from({length: n}, (v, k) => (k + 1) * x)
+
+
+const arr1 = Array.from({ length: 5 }, (v, k) => k);
+console.log(arr1); // [0, 1, 2, 3, 4]
+
+
+const arr2 = Array.from([1, 2, 3], (x) => x + x);
+console.log(arr2); // [2, 4, 6]
+
+
+const arr3 = Array.from({ length: 5 }, () => "");
+console.log(arr3); // ["", "", "", "", ""]
+
