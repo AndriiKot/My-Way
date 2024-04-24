45a435891ddaab1b4767960e13261da82563854a
AndriiKot <a_kocyuba@ukr.net>
2024-04-24 18:27:36 +0200

    js docs 'max elment not sort array'

---
+++ FRONTEND/__JAVASCRIPT__/__cooks__/Array/__reduce__/__max_elments_not_sort.js
@@ -0,0 +1,12 @@
+﻿
+const arr = Array.from({length: 1_000_000}, () => 0);
+
+// console.log(arr);  test
+
+arr[1_000_000] = 100;
+
+// !!! const max = Math.max(...arr);   RangeError: Maximum call stack size exceeded   !!!
+
+const max = arr.reduce((a,b) => (a > b ? a : b));
+console.log(max);
+
