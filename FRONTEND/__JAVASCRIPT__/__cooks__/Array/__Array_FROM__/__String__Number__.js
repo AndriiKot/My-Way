03a81ba0308b4f452bd7116daea596bdcc658063
AndriiKot <a_kocyuba@ukr.net>
2024-05-08 19:58:07 +0200

    js docs

---
+++ FRONTEND/__JAVASCRIPT__/__cooks__/Array/__Array_FROM__/__String__Number__.js
@@ -0,0 +1,15 @@
+function digitize1(n) {
+  return Array.from(String(n), Number).reverse();
+}
+
+// sample tests
+console.log(digitize1(35231), [1, 3, 2, 5, 3]);
+
+// Convert number to reversed array of digits
+// Given a random non-negative number,
+// you have to return the digits of this
+// number within an array in reverse order.
+
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
