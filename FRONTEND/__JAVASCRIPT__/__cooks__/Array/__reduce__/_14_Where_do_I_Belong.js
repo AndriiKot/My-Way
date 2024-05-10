27aa10c1b79e22d6f6938f48da97542c1021d6ae
AndriiKot <a_kocyuba@ukr.net>
2024-05-10 10:22:01 +0200

    js docs

---
+++ FRONTEND/__JAVASCRIPT__/__cooks__/Array/__reduce__/_14_Where_do_I_Belong.js
@@ -0,0 +1,16 @@
+﻿function getIndexToIns(arr, num) {
+  const newArray = arr.reduce((accumulator, currentValue) => {
+    accumulator.push(currentValue);
+
+    if (currentValue <= num) {
+      accumulator.push(num);
+    }
+    
+    return accumulator;
+  }, []);
+  return newArray;
+}
+
+getIndexToIns([40, 60], 50);
+
+console.log(getIndexToIns([40, 60], 50));
---
+++ FRONTEND/__JAVASCRIPT__/__cooks__/Strings/__replace__regular__2__.js
@@ -0,0 +1,6 @@
+function fakeBin1(x) {
+  return x.replace(/\d/g, (d) => (d < 5 ? 0 : 1));
+}
+
+
+
