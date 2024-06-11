dfa838ac0c3e49212347ad398f0a1cbcb9ac62bd
AndriiKot <a_kocyuba@ukr.net>
2024-06-11 09:43:06 +0200

    Grunt and less cooks

---
+++ FRONTEND/Task_Runner/GRUNT/_TOP__cooks__/Grunt_watch_and_less_/Gruntfile.js
@@ -0,0 +1,31 @@
+﻿module.exports = function (grunt) {
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+         optimization: 2,
+        },
+        files: {
+          "./main.css": "./less_css/main.less",
+        },
+      },
+    },
+    watch: {
+      styles: {
+        files: ["less_css/**/*.less"], 
+        tasks: ["less"],
+        options: {
+          nospawn: true,
+        },
+      },
+    },
+  });
+
+  grunt.registerTask("default", ["watch"]);
+};
