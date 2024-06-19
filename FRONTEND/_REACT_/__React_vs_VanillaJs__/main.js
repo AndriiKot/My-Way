5962e0378f41adcd7b1bf33b0b3139c2419a37c3
AndriiKot <a_kocyuba@ukr.net>
2024-06-19 10:19:17 +0200

    js docs react vs vanillajs

---
+++ FRONTEND/_REACT_/__React_vs_VanillaJs__/main.js
@@ -0,0 +1,24 @@
+﻿"use strict";
+
+const container1 = document.querySelector(".container-1");
+const container2 = document.querySelector(".container-2");
+
+// VanillaJS
+const buttonVanillaJs = document.createElement("button");
+buttonVanillaJs.textContent = "Vanilla JS"; 
+// or buttonVanillaJs.innerText = "Vanilla JS";
+
+container1.append(buttonVanillaJs);
+// or container1.appendChild(buttonVanillaJs);
+
+
+// React
+const root = ReactDOM.createRoot(container2);
+const buttonReact = React.createElement("button", {}, "React");
+
+// TESTING
+container2.append(buttonReact);
+root.render(buttonReact);
+
+
+console.log(buttonVanillaJs, buttonReact);
