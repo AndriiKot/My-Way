594a3acdb23d67127137075b11a72e360c9ecb71
AndriiKot <a_kocyuba@ukr.net>
2024-06-19 09:24:50 +0200

    DOM docs

---
+++ FRONTEND/__DOM__/__innerText__textContent.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+const container1 = document.querySelector(".container-1");
+const container2 = document.querySelector(".container-2");
+
+
+const buttonVanillaJs = document.createElement("button");
+buttonVanillaJs.textContent = "Vanilla JS"; 
+// or buttonVanillaJs.innerText = "Vanilla JS";
+
+container2.appendChild(buttonVanillaJs);
+// or container1.append(buttonVanillaJs);
+
+
+
+
