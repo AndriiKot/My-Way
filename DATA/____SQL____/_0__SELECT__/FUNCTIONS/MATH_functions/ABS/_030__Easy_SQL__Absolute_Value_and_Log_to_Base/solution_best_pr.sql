a797101e6988b66c17692c5d4ffca9e5399181d6
AndriiKot <a_kocyuba@ukr.net>
2024-07-18 10:30:14 +0200

    sql docs and css docs

--- DATA/____SQL____/_0__SELECT__/FUNCTIONS/MATH_functions/ABS/_030__Easy_SQL__Absolute_Value_and_Log_to_Base/solution_best_pr.sql
+++ DATA/____SQL____/_0__SELECT__/FUNCTIONS/MATH_functions/ABS/_030__Easy_SQL__Absolute_Value_and_Log_to_Base/solution_best_pr.sql
@@ -1,18 +1,5 @@
-SELECT count(id) as products, country
-FROM products
-WHERE country in ('United States of America', 'Canada')
-GROUP BY country
-ORDER BY products desc
-
-
-
--- MY SOLUTION
-
 SELECT 
-    country,
-    count(country) AS products
-FROM products
-WHERE country IN ('United States of America', 'Canada')
-GROUP BY country
-ORDER BY count(country) DESC
+    ABS(number1) AS abs, 
+    LOG(64, number2) AS log
+FROM decimals;
 
